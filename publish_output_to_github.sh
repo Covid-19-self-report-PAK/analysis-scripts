e64b0fcaf6fcec1e82e9cc7de483808462fe09f2
wakkarkhan <muhammadwakkar777@gmail.com>
2020-04-03 03:35:58 +0500

    update 2020-04-02T22:35:58Z

--- publish_output_to_github.sh
+++ publish_output_to_github.sh
@@ -5,19 +5,19 @@
 # - you need to check the path...they are relative
 
 # load .env vars
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
+# if [ -f .env ]
+# then
+#   export $(cat .env | sed 's/#.*//g' | xargs)
+# fi
 
-OUTPUT_DIR='output_datasets/.'
+# OUTPUT_DIR='output_datasets/.'
 
-# copy files
-cp -R $OUTPUT_DIR/daily-reports $PUBLIC_DATASETS_REPO_RELATIVE_PATH
-cp $OUTPUT_DIR/merge-all-days.csv $PUBLIC_DATASETS_REPO_RELATIVE_PATH
+# # copy files
+# cp -R $OUTPUT_DIR/daily-reports $PUBLIC_DATASETS_REPO_RELATIVE_PATH
+# cp $OUTPUT_DIR/merge-all-days.csv $PUBLIC_DATASETS_REPO_RELATIVE_PATH
 
-# move to the repo
-cd $PUBLIC_DATASETS_REPO_RELATIVE_PATH
+# # move to the repo
+# cd $PUBLIC_DATASETS_REPO_RELATIVE_PATH
 
 # print the timestamp (needed for the frontend)
 now=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
